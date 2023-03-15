bf99597c255f7450a56d2ec5d147b342ec2277cb
yun0011 <hiyun01@naver.com>
2023-03-15 16:53:18 +0900

    final_1

--- Django/coffee/mysite/config/config/settings.py
+++ Django/coffee/mysite/config/config/settings.py
@@ -84,14 +84,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME' : 'csv_db',
-        'USER' : 'root',
-        'PASSWORD' : '1234',
-        'HOST' : '127.0.0.1',
-        'PORT' : '3305',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME' : 'csv_db',
+        # 'USER' : 'root',
+        # 'PASSWORD' : '1234',
+        # 'HOST' : '127.0.0.1',
+        # 'PORT' : '3305', 
     }
 }
 
